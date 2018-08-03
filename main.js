d207aaa433bf383bb1015c440c968e0965d9e371
ricky-bruner <alteredecho07@yahoo.com>
2018-08-03 15:37:31 -0500

    omg it works

--- main.js
+++ main.js
@@ -9,19 +9,19 @@ button.addEventListener("click", function(){
 
 //print the junior instructors to the DOM
 let jrInstructors = document.getElementById("jr-instructors")
-staff.forEach(jrInstructor => {
-    jrInstructors.appendChild(`${jrInstructor} is a junior instructor at NSS`);
+staff.juniorInstructors.forEach(jrInstructor => {
+    jrInstructors.innerHTML +=`<p>${jrInstructor} is a junior instructor at NSS</p>`;
 });
 
 
 //console log each of the schools in the school array individually and console log them
-schools.forEach(schools => {
-    console.log(schools);
+schools.forEach(school => {
+    console.log(school);
 });
 
 
 //print each of the lead instructors to the console
-for(let i = 0; i <= staff.instructors.length; i++){
+for(let i = 0; i < staff.instructors.length; i++){
     console.log(staff.instructors[i]);
 }
 
@@ -29,7 +29,8 @@ for(let i = 0; i <= staff.instructors.length; i++){
 //print the result to the console
 let parkInfo = {};
 fetch("https://external-data-56670.firebaseio.com/park-info.json")
+.then(res => res.json())
 .then(result => {
     parkInfo = result;
+    console.log(parkInfo);
 });
-console.log(parkInfo);
